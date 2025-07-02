f6069eea42b63fce5da386a7c9688c1f4ac47850
ddudi <ddudios@naver.com>
2025-07-03 00:52:02 +0900

    refactor: 중복 코드 줄이기 - Outlet Collection과 배열을 활용해 중복 코드 제거 - tag 활용으로 중복 코드 제거
    - 열거형으로  Assets의 이미지 이름 안전하게 사용

--- EmotionDiary/ViewController.swift
+++ EmotionDiary/ViewController.swift
@@ -7,65 +7,39 @@
 
 import UIKit
 
+// 열거형 사용해서 안전하게
+enum ImageName: String {
+    case happy = "mono_slime1"
+    case like = "mono_slime2"
+    case love = "mono_slime3"
+    case angry = "mono_slime4"
+    case bored = "mono_slime5"
+    case stuffy = "mono_slime6"
+    case panic = "mono_slime7"
+    case pathetic = "mono_slime8"
+    case sad = "mono_slime9"
+}
+
 class ViewController: UIViewController {
 
-    var angryCount = 0
+    var emotionLabelText = ["행복해", "좋아해", "사랑해", "화가나", "심심해", "답답해", "당황해", "한심해", "속상해"]
+    let emotionButtonImageName = [ImageName.happy.rawValue, ImageName.like.rawValue, ImageName.love.rawValue, ImageName.angry.rawValue, ImageName.bored.rawValue, ImageName.stuffy.rawValue, ImageName.panic.rawValue, ImageName.pathetic.rawValue, ImageName.sad.rawValue]
+    
+    var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     
     // 아웃렛 컬렉션으로 관리해보기
-    @IBOutlet var happyLabel: UILabel!
-    @IBOutlet var likeLabel: UILabel!
-    @IBOutlet var loveLabel: UILabel!
-    
-    @IBOutlet var angryLabel: UILabel!
-    @IBOutlet var boredLabel: UILabel!
-    @IBOutlet var stuffyLabel: UILabel!
-    
-    @IBOutlet var panicLabel: UILabel!
-    @IBOutlet var patheticLabel: UILabel!
-    @IBOutlet var sadLabel: UILabel!
-    
-    @IBOutlet var happyButton: UIButton!
-    @IBOutlet var likeButton: UIButton!
-    @IBOutlet var loveButton: UIButton!
-    
-    @IBOutlet var angryButton: UIButton!
-    @IBOutlet var boredButton: UIButton!
-    @IBOutlet var stuffyButton: UIButton!
-    
-    @IBOutlet var panicButton: UIButton!
-    @IBOutlet var patheticButton: UIButton!
-    @IBOutlet var sadButton: UIButton!
+    @IBOutlet var emotionLabels: [UILabel]!
+    @IBOutlet var emotionButtons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .lightGray
         
-        designLabelUI(label: happyLabel, text: "행복해", count: 0)
-        designLabelUI(label: likeLabel, text: "좋아해", count: 0)
-        designLabelUI(label: loveLabel, text: "사랑해", count: 0)
-        
-        designLabelUI(label: angryLabel, text: "화가나", count: 0)
-        designLabelUI(label: boredLabel, text: "심심해", count: 0)
-        designLabelUI(label: stuffyLabel, text: "답답해", count: 0)
-        
-        designLabelUI(label: panicLabel, text: "당황해", count: 0)
-        designLabelUI(label: patheticLabel, text: "한심해", count: 0)
-        designLabelUI(label: sadLabel, text: "속상해", count: 0)
-        
-        designButtonUI(button: happyButton, image: "mono_slime1")
-        designButtonUI(button: likeButton, image: "mono_slime2")
-        designButtonUI(button: loveButton, image: "mono_slime3")
-        
-        designButtonUI(button: angryButton, image: "mono_slime4")
-        designButtonUI(button: boredButton, image: "mono_slime5")
-        designButtonUI(button: stuffyButton, image: "mono_slime6")
-        
-        designButtonUI(button: panicButton, image: "mono_slime7")
-        designButtonUI(button: patheticButton, image: "mono_slime8")
-        designButtonUI(button: sadButton, image: "mono_slime9")
-        
-        
+        for index in 0...8 {
+            designLabelUI(label: emotionLabels[index], text: emotionLabelText[index], count: 0)
+            designButtonUI(button: emotionButtons[index], image: emotionButtonImageName[index])
+        }
     }
 
     func designLabelUI(label: UILabel, text: String, count: Int) {
@@ -79,43 +53,12 @@ class ViewController: UIViewController {
         button.imageView?.layer.transform = CATransform3DMakeScale(0.3, 0.3, 0.3)
     }
     
-    @IBAction func happyButtonTapped(_ sender: UIButton) {
-        let count = Int.random(in: 1...10)
-        designLabelUI(label: happyLabel, text: "행복해", count: count)
+    @IBAction func getRandomNumber(_ sender: UIButton) {
+        designLabelUI(label: emotionLabels[sender.tag], text: emotionLabelText[sender.tag], count: Int.random(in: 1...10))
     }
     
-    @IBAction func likeButtonTapped(_ sender: UIButton) {
-        designLabelUI(label: likeLabel, text: "좋아해", count: Int.random(in: 1...10))
-    }
-    
-    @IBAction func loveButtonTapped(_ sender: UIButton) {
-        designLabelUI(label: loveLabel, text: "사랑해", count: Int.random(in: 1...10))
-    }
-    
-    @IBAction func angryButtonTapped(_ sender: UIButton) {
-        angryCount += 1
-        designLabelUI(label: angryLabel, text: "화가나", count: angryCount)
-    }
-    
-    @IBAction func boredButtonTapped(_ sender: UIButton) {
-        designLabelUI(label: boredLabel, text: "심심해", count: Int.random(in: 1...10))
-    }
-    
-    @IBAction func stuffyButtonTapped(_ sender: UIButton) {
-        designLabelUI(label: stuffyLabel, text: "답답해", count: Int.random(in: 1...10))
-    }
-    
-    @IBAction func panicButtonTapped(_ sender: UIButton) {
-        designLabelUI(label: panicLabel, text: "당황해", count: Int.random(in: 1...10))
-    }
-    
-    @IBAction func patheticButtonTapped(_ sender: UIButton) {
-        designLabelUI(label: patheticLabel, text: "한심해", count: Int.random(in: 1...10))
-    }
-    
-    @IBAction func sadButtonTapped(_ sender: UIButton) {
-        designLabelUI(label: sadLabel, text: "속상해", count: Int.random(in: 1...10))
+    @IBAction func countNumber(_ sender: UIButton) {
+        counts[sender.tag] += 1
+        designLabelUI(label: emotionLabels[sender.tag], text: emotionLabelText[sender.tag], count: counts[sender.tag])
     }
 }
-
-// 열거형사용해서 안전하게
