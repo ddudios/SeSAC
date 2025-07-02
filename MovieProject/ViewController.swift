20e27d85b5ea0c4f8b96328c5160029355fbafdc
ddudi <ddudios@naver.com>
2025-07-02 16:26:46 +0900

    Initial commit

---
+++ MovieProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieProject
+//
+//  Created by Suji Jang on 7/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
