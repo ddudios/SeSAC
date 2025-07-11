094a6142136fa55e31530d2cfcc5307512028e74
ddudi <ddudios@naver.com>
2025-07-11 19:01:35 +0900

    feat: Magazine Information View - design: Storyboard - Custom Table View 활용 - 더미 데이터 구조체 활용 - Kingfisher 라이브러리를 활용해 이미지 로드 - DateFormatter를 활용해 String타입의 날짜를 원하는 포멧으로 변경: String -> Date -> String

---
+++ Travel/TravelMagazineTableViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  TravelNewsTableViewCell.swift
+//  Travel
+//
+//  Created by Suji Jang on 7/11/25.
+//
+
+import UIKit
+
+struct CustomFont {
+    static let title = UIFont.systemFont(ofSize: 28, weight: .semibold)
+    static let subtitle = UIFont.systemFont(ofSize: 18, weight: .medium)
+    static let caption = UIFont.systemFont(ofSize: 15, weight: .medium)
+}
+
+struct CornerRadiusValue {
+    static let newsImage: CGFloat = 20
+}
+
+class TravelMagazineTableViewCell: UITableViewCell {
+    
+    @IBOutlet var travelMagazineImageView: UIImageView!
+    @IBOutlet var travelMagazineTitleLabel: UILabel!
+    @IBOutlet var travelMagazinesubtitleLabel: UILabel!
+    @IBOutlet var travelMagazineDateLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        travelMagazineImageView.layer.cornerRadius = CornerRadiusValue.newsImage
+        travelMagazineImageView.clipsToBounds = true
+        travelMagazineImageView.backgroundColor = .black
+        
+        travelMagazineTitleLabel.font = CustomFont.title
+        travelMagazineTitleLabel.textColor = .black
+        travelMagazineTitleLabel.numberOfLines = 0
+        travelMagazineTitleLabel.text = """
+2024 NEWS
+해외여행, 이렇게 달라졌다!
+"""
+        
+        travelMagazinesubtitleLabel.font = CustomFont.subtitle
+        travelMagazinesubtitleLabel.textColor = .gray
+        travelMagazinesubtitleLabel.text = "알고 준비하는 2024 새 여행"
+        
+        travelMagazineDateLabel.font = CustomFont.caption
+        travelMagazineDateLabel.textColor = .gray
+        travelMagazineDateLabel.textAlignment = .right
+        travelMagazineDateLabel.text = "23년 11월 18일"
+    }
+}
---
+++ Travel/TravelMagazineTableViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TravelNewsTableViewController.swift
+//  Travel
+//
+//  Created by Suji Jang on 7/11/25.
+//
+
+import UIKit
+import Kingfisher
+
+class TravelMagazineTableViewController: UITableViewController {
+    
+    let magazineInfo = MagazineInfo()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return magazineInfo.magazine.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "travelNewsCell", for: indexPath) as! TravelMagazineTableViewCell
+        
+        let magazineIndexPathRow = magazineInfo.magazine[indexPath.row]
+        let url = URL(string: magazineIndexPathRow.photo_image)
+        cell.travelMagazineImageView.kf.setImage(with: url)
+        cell.travelMagazineTitleLabel.text = magazineIndexPathRow.title
+        cell.travelMagazinesubtitleLabel.text = magazineIndexPathRow.subtitle
+        cell.travelMagazineDateLabel.text = magazineIndexPathRow.date
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 510
+    }
+    
+    func configureUI() {
+        configureNavigationBar()
+    }
+    
+    func configureNavigationBar() {
+        self.navigationItem.title = "SeSAC TRAVEL"
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .white
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+        
+        navigationController?.navigationBar.tintColor = .gray
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+    }
+}
