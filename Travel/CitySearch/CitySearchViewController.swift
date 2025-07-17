f5cbf60e99fa4ebb808e04fd5c54899ce052707f
ddudi <ddudios@naver.com>
2025-07-18 00:31:39 +0900

    feat: [City Search Table View] Segmented Control + filter - 데이터 다루기

--- Travel/CitySearch/CitySearchViewController.swift
+++ Travel/CitySearch/CitySearchViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 class CitySearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var cityListTableView: UITableView!
+    @IBOutlet var filterSegmentedControl: UISegmentedControl!
     
-    let city = CityInfo().city
+    var cityList = CityInfo().city
     
     let citySearchDetailViewController = "CitySearchDetailViewController"
     let storyboardIdentifier = "CitySearchView"
@@ -22,9 +23,43 @@ class CitySearchViewController: UIViewController, UITableViewDelegate, UITableVi
         configureUI()
     }
     
+    @IBAction func filterSegmentedControlValueChanged(_ sender: UISegmentedControl) {
+        let all = CityInfo().city
+        switch sender.selectedSegmentIndex {
+        case 0:
+            cityList = all
+        case 1:
+            var domesticArray: [City] = []
+            for row in all {
+                if row.domestic_travel {
+                    domesticArray.append(row)
+                }
+            }
+            cityList = domesticArray
+        case 2:
+            var abroadArray: [City] = []
+            for row in all {
+                if !row.domestic_travel {
+                    abroadArray.append(row)
+                }
+            }
+            cityList = abroadArray
+        default:
+            print("error: \(#function)")
+        }
+        cityListTableView.reloadData()
+    }
+    
     func configureUI() {
         configureNavigationBarUI(title: "인기 도시")
+        configureSegmentedControl()
         configureTableView()
+    }
+    
+    func configureSegmentedControl() {
+        filterSegmentedControl.setTitle("모두", forSegmentAt: 0)
+        filterSegmentedControl.setTitle("국내", forSegmentAt: 1)
+        filterSegmentedControl.insertSegment(withTitle: "해외", at: 2, animated: true)
     }
     
     func configureTableView() {
@@ -39,12 +74,12 @@ class CitySearchViewController: UIViewController, UITableViewDelegate, UITableVi
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return city.count
+        return cityList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = cityListTableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! CitySearchTableViewCell
-        let row = city[indexPath.row]
+        let row = cityList[indexPath.row]
         
         let url = URL(string: row.city_image)
         cell.cityImageView.kf.setImage(with: url)
@@ -59,7 +94,7 @@ class CitySearchViewController: UIViewController, UITableViewDelegate, UITableVi
         let storyboard = UIStoryboard(name: storyboardIdentifier, bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: citySearchDetailViewController) as! CitySearchDetailViewController
         
-        vc.data = city[indexPath.row]
+        vc.data = cityList[indexPath.row]
         
         navigationController?.pushViewController(vc, animated: true)
     }
