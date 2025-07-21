3d11ea06f7b20de3610126a38f3478a1322418b2
ddudi <ddudios@naver.com>
2025-07-22 00:56:30 +0900

    Design: NPay View - NPayViewController 생성 - Root ViewController NPay ViewController로 설정 - Color Extension 추가

---
+++ SeSAC7Week1Remind/SeSAC7Week1Remind/View/NPayViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NPayViewController.swift
+//  SeSAC7Week1Remind
+//
+//  Created by Suji Jang on 7/22/25.
+//
+
+import UIKit
+
+final class NPayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .nPayMain
+    }
+}
