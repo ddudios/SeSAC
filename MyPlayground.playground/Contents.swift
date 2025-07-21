497accedab3b100a65017fe3ebf13335f98b5e1f
ddudi <ddudios@naver.com>
2025-07-21 17:37:18 +0900

    [Week1] 2025.06.30 (월)

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,3 @@
+var onlineService = "WhaleOn"
+var notice = ##"온라인 라이브 수업은 \##(onlineService)으로 진행합니다."##
+print(notice)
---
+++ SeSAC7Week1/SeSAC7Week1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC7Week1
+//
+//  Created by Suji Jang on 6/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
