d7a0183582f462b81a5e8169dc052f57a4bb597b
ddudi <ddudios@naver.com>
2025-07-26 01:40:37 +0900

    파일 생성

---
+++ Week4/20250725/ShoppingShopping/ShoppingShopping/Protocol/ViewDesignProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ViewDesignProtocol.swift
+//  ShoppingShopping
+//
+//  Created by Suji Jang on 7/26/25.
+//
+
+import Foundation
---
+++ Week4/20250725/ShoppingShopping/ShoppingShopping/Search/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  ShoppingShopping
+//
+//  Created by Suji Jang on 7/26/25.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
